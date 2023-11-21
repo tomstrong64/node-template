db6a62e200c04489b1ed769222fc1ee27210f4d9
tomstrong64 <tom.strong64@gmail.com>
2023-11-21 16:00:56 +0000

    Add healthcheck and db connection

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,17 +1,58 @@
 import { createServer } from 'http';
+import mongoose from 'mongoose';
+
+import {} from 'dotenv/config';
+
 import app from '../app.js';
 
+// connet to db
+await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+console.log('MongoDB connected');
+
+// disable autoIndex in production
+if (process.env.NODE_ENV === 'production') mongoose.set('autoIndex', false);
+
+// create server
 const server = createServer(app);
-
-// TODO: setup database connections, etc.
-
 server.listen(3000);
+console.log('Server listening on port 3000');
 
-// graceful shutdown
-process.on('SIGTERM', () => {
-    server.close(() => {
-        console.log('Process terminated');
+//////////////
+// SHUTDOWN //
+//////////////
+
+// handle mongoose connection error
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+    process.exit(1);
+});
+
+// handle mongoose disconnect
+mongoose.connection.on('disconnected', () => {
+    console.error('MongoDB disconnected');
+    process.exit(1);
+});
+
+// shutdown server
+const gracefulShutdown = () => {
+    server.close(async () => {
+        console.log('Server closed');
+        await mongoose.connection.close();
+        console.log('MongoDB connection closed');
+        process.exit(0);
     });
+};
 
-    // TODO: close database connections, etc.
+// handle shutdown message
+process.on('SIGTERM', () => {
+    console.log('SIGTERM received');
+    gracefulShutdown();
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT received');
+    gracefulShutdown();
 });
